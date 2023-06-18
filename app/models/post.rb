111e135da44bef3bfd220c3aa921aef8064d69bf
Humberto <hdkaratechop@gmail.com>
2023-06-17 23:52:03 -0700

    add validations to tilte and body presence/length

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    belongs_to :user 
+    validates :title, :body, presence: true  
+    validates :title, length: {minimum: 1, maximum: 300}
+    validates :body, length: {maximum:40000}
 end
